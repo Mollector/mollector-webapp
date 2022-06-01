af241b1b4a5c146454d9b1896952f2477102ef1e
Theo Dao <trungdao912@gmail.com>
2022-06-01 16:26:23 +0700

    Updates

--- static/js/4.3a640e9f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmollector-webapp"]=this["webpackJsonpmollector-webapp"]||[]).push([[4],{848:function(p,o){}}]);
-//# sourceMappingURL=4.3a640e9f.chunk.js.map
---
+++ static/js/4.92d48782.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmollector-webapp"]=this["webpackJsonpmollector-webapp"]||[]).push([[4],{849:function(p,o){}}]);
+//# sourceMappingURL=4.92d48782.chunk.js.map
